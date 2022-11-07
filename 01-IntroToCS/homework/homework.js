53f00f72d81c5c2f249b8e6e941cc3e17bf47d61
FacuTombesi <facundomtombesi@gmail.com>
2022-11-07 15:10:28 -0300

    01-homework

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -2,14 +2,43 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
+  
+  var decimal = 0;
 
+  for(var i = 0; i < num.length; i++) {
+    decimal += num[i] * 2 ** (num.length - 1 - i);
+  }
+
+ // for(var i = 0; i < num.length; i++) {
+ //    decimal = decimal + num[i] * Math.pow(2, num.length - 1 - i);
+ // }
+
+  return decimal;
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
+  
+  var binario = [];
+   
+  while(num >= 1) {
+     binario.unshift(num % 2);
+     num = (num - (num % 2)) / 2;
+  }
 
+  return binario.join("");
+  
+  // var binario = (num % 2).toString();
+
+  // for (; num > 1; ) {
+  //    num = parseInt(num / 2);
+  //    binario = (num % 2) + (binario);
+  // }
+
+  // return binario;
 }
 
+   
 
 module.exports = {
   BinarioADecimal,
